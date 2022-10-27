fd20a05ed3ac1a166189dd866c78166d76870651
tamas.molnar <tamas.molnar@liferay.com>
2022-10-27 10:30:10 +0200

    Config

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@ module.exports = {
   name: "wallet-connect-expo",
   slug: "wallet-connect-expo",
   version: '1.0.0',
-  "expo": {
-    "extra": {
+  expo: {
+    extra: {
       "eas": {
         "projectId": "89f318bf-ccfc-43b4-aa1a-a62fde8099ea"
       },
@@ -11,7 +11,7 @@ module.exports = {
       TENDERLY_PROJECT: process.env.TENDERLY_PROJECT,
       TENDERLY_ACCESS_KEY: process.env.TENDERLY_ACCESS_KEY,
     },
-    "ios": {
+    ios: {
       "bundleIdentifier": "com.buidlguidl.scaffoldwallet"
     }
   }
