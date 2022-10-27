c3d3d8da622b4cfc1f14d40fc030e87a37fe93cf
tamas.molnar <tamas.molnar@liferay.com>
2022-10-27 10:21:53 +0200

    Config

--- app.config.js
+++ app.config.js
@@ -9,5 +9,8 @@ module.exports = {
     TENDERLY_USER: process.env.TENDERLY_USER,
     TENDERLY_PROJECT: process.env.TENDERLY_PROJECT,
     TENDERLY_ACCESS_KEY: process.env.TENDERLY_ACCESS_KEY,
+  },
+  "ios": {
+    "bundleIdentifier": "com.buidlguidl.scaffoldwallet"
   }
 };
